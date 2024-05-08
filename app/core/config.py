993d851fa1002f2c4e37721e6ba500e2b6097383
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2024-05-08 19:28:26 +0530

    Updated ehr url

--- app/core/config.py
+++ app/core/config.py
@@ -30,7 +30,7 @@ class Settings():
 
     ORTHANC_URL = "https://pacs.radassist.ai"
     # ORTHANC_URL = "http://35.202.115.123:8042"
-    FHIR_URL = "https://ehr.radassist.ai/hapi-fhir-jpaserver/fhir"
+    FHIR_URL = "https://ehr.radassist.ai/fhir"
     # API_URL = "http://ehr.iradreports.com/hapi-fhir-jpaserver/fhir"
     # API_URL = "http://localhost:8888/api/v1"
     API_URL = "https://api.deepmd.io/api/v1"
